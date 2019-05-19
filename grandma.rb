89145827bdd46107aac48b518ca7abcdc9d2585c
Ryan Bravo <rrbravo2@gmail.com>
2019-05-19 06:27:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,11 @@
 def speak_to_grandma(speak)
- if 
+ if speak == "I LOVE YOU GRANDMA!"
+   return "I LOVE YOU TOO PUMPKIN!"
+  elsif speak == speak.upcase
+   return "NO, NOT SINCE 1938!"
+  else 
+    return "HUH?! SPEAK UP, SONNY"
+   end 
 end
 # Write a speak_to_grandma method.
 
